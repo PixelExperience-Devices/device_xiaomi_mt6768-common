37fbb8a997155ae854fb6ba76cfc8886b9624456
LuK1337 <priv.luk@gmail.com>
2021-02-03 19:43:03 +0600

    merlin: Drop android.hardware.thermal HAL
    * android.hardware.thermal@1.0::IThermal/default is deprecated in compatibility matrix at FCM Version 4; it should not be served.
    
    Change-Id: I2e6cf8206dc592684827c371ab5d6a44d5584afb

--- device.mk
+++ device.mk
@@ -167,11 +167,6 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     libsensorndkbridge
-    
-# Thermal
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service
 
 # TinyXML
 PRODUCT_PACKAGES += \
