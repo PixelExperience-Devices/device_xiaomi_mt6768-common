e861c4bf014b0b4a8ab1fae466a7438fc6907613
Zinadin Zidan <zidan44@pixelexperience.org>
2021-05-24 15:43:31 +0600

    mt6768-common: sepolicy: Use new path for common sepolicy
    * Ref: https://gerrit.pixelexperience.org/c/device_mediatek_sepolicy/+/11507
    
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: I6358b15c147df8f802fd69303044507b72996555

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -143,7 +143,7 @@ TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
 # Sepolicy
-include device/mediatek/sepolicy/sepolicy.mk
+include device/mediatek/sepolicy_vndr/SEPolicy.mk
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
