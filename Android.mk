0596d5deeafe88ca6ccbecf483c45d75e4097e9a
Matsvei Niaverau <matvej2002@gmail.com>
2021-09-05 10:58:22 +0300

    mt6768-common: Add support for merlinx
    Since codename "merlin" exists (Moto G3 Turbo), we should rebrand our device as merlinx for some ROM's

--- Android.mk
+++ Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter merlin lava,$(TARGET_DEVICE)),)
+ifneq ($(filter merlin merlinx lava,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 include $(CLEAR_VARS)
 
