c058418047f6e39a83c9253e0e32781e435ba4b3
Matsvei Niaverau <matvej2002@gmail.com>
2021-10-11 20:58:03 +0300

    mt6768-common: Build additional libs for camera HAL
    vndksupport: Could not load /vendor/lib64/hw/android.hardware.camera.provider@2.6-impl-mediatek.so from default namespace: dlopen failed: library "android.hardware.camera.device@3.5.so" not found: needed by /vendor/lib64/libmtkcam_device3_hidl.so in namespace (default).

--- mt6768-common.mk
+++ mt6768-common.mk
@@ -89,7 +89,17 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.5 \
     android.hardware.camera.provider@2.5.vendor \
     android.hardware.camera.provider@2.6 \
-    android.hardware.camera.provider@2.6.vendor
+    android.hardware.camera.provider@2.6.vendor \
+    android.hardware.camera.device@3.2 \
+    android.hardware.camera.device@3.2.vendor \
+    android.hardware.camera.device@3.3 \
+    android.hardware.camera.device@3.3.vendor \
+    android.hardware.camera.device@3.4 \
+    android.hardware.camera.device@3.4.vendor \
+    android.hardware.camera.device@3.5 \
+    android.hardware.camera.device@3.5.vendor \
+    android.hardware.camera.device@3.6 \
+    android.hardware.camera.device@3.6.vendor
     
 # Display
 PRODUCT_PACKAGES += \
