be2647f9a0bcefc6deca6bdd96f3dc88dae690ea
Matsvei Niaverau <matvej2002@gmail.com>
2021-12-13 20:37:26 +0300

    mt6768-common: rootdir: Move init project back to vendor
    * Should be different for current devices

--- mt6768-common.mk
+++ mt6768-common.mk
@@ -347,7 +347,6 @@ PRODUCT_PACKAGES += \
     init.mt6768.rc \
     init.mt6768.usb.rc \
     init.wlan_drv.rc \
-    init.project.rc \
     init.sensor_1_0.rc \
     meta_init.connectivity.rc \
     meta_init.modem.rc \
