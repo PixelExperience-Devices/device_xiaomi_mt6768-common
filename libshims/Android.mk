69a31654572644d5bdb7948942d313cf5abe41bc
Arian <arian.kulmer@web.de>
2021-12-26 20:02:09 +0200

    mt6768-common: ims: Properly shim libmtk_vt_service.so
    As pointed out by @ R0rt1z2, the previous shims used to break ViLTE
    functionality, so replace them with the proper ones
    
    Originally from
    * https://github.com/realme-mt6785-devs/android_device_realme_RM6785/blob/lineage-19.0/libshims
    * https://github.com/realme-mt6785-devs/android_device_realme_RM6785/blob/lineage-19.0/libshims/libshim_vtservice.cpp
    
    Co-authored-by: SamarV-121 <samarvispute121@pm.me>
    Co-authored-by: R0rt1z2 <rogerortizleal@gmail.com>
    Co-authored-by: Kyle Harrison <khwebmail@gmail.com>
    Signed-off-by: LinkBoi00 <50962670+LinkBoi00@users.noreply.github.com>
    Change-Id: I109db7220b47e2d6b174bfd176edf6e7449324aa

--- libshims/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := libshim_vtservice.cpp
-LOCAL_MODULE := libshim_vtservice
-LOCAL_MODULE_TAGS := optional
-
-include $(BUILD_SHARED_LIBRARY)
