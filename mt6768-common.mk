23a5518fed48d5f6fdf0b9b3675c521b240a4bf9
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-01-18 19:53:39 +0600

    mt6768-common: Uprev bluetooth audio HIDL impl to 2.1
    * We get free LC3 support, isn't it poggers?
    
    Change-Id: I42c3cea0bb0e63f87c37c73f5c9f736048f71755

--- mt6768-common.mk
+++ mt6768-common.mk
@@ -40,7 +40,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio@6.0-util.vendor \
     android.hardware.audio.common@6.0-util \
-    android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth.audio@2.1-impl \
     audio.a2dp.default \
     audio.bluetooth.default \
     audio.r_submix.default \
