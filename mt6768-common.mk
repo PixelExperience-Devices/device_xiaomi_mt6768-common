47c469b106d3eb50946851c30b3cdd77d7dd2826
Matsvei Niaverau <matvej2002@gmail.com>
2022-03-23 20:08:51 +0300

    mt6768-common: Fall back to Q power hal
    * Surprisingly, but Q power hal works better than the new one. So let's use it.
    
    Change-Id: I709dc6e3b9114496ce9f7a72a932559aca72f4cb

--- mt6768-common.mk
+++ mt6768-common.mk
@@ -218,7 +218,6 @@ PRODUCT_COPY_FILES += \
 
 # Perf
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/perf/power_app_cfg.xml:$(TARGET_COPY_OUT_VENDOR)/etc/power_app_cfg.xml \
     $(LOCAL_PATH)/configs/perf/powercontable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powercontable.xml \
     $(LOCAL_PATH)/configs/perf/powerscntbl.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerscntbl.xml
 
