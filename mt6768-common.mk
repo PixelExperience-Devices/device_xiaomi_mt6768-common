a332eef69707bab414d3c4db2c8836dc2987d46d
Matsvei Niaverau <matvej2002@gmail.com>
2022-06-20 15:47:55 +0000

    mt6768-common: Drop separated IMS repository
    * Not needed anymore
    
    Change-Id: I50d295790372971e8f1dea4fe9fe51f2ead30ae3

--- mt6768-common.mk
+++ mt6768-common.mk
@@ -19,9 +19,6 @@ COMMON_PATH := device/xiaomi/mt6768-common
 # Include Dev GSI Keys
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
-# Get non-open-source specific aspects
-$(call inherit-product, vendor/xiaomi/mt6768-ims/mt6768-ims-vendor.mk)
-
 # APNs
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/apns-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
