eb811a074a160b074233d744cf606d6883180247
bengris32 <bengris32@protonmail.ch>
2022-07-21 21:23:17 +0100

    mt6768-common: power: Build custom Power AIDL service library * Build a custom Power AIDL service library that acts as a   drop-in replacement for MediaTek's one to fix PowerHAL   INTERACTION hint issues.
    * It could also be modified to support other AOSP power hints.
    
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- mt6768-common.mk
+++ mt6768-common.mk
@@ -260,7 +260,7 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.1.vendor \
     android.hardware.power@1.2.vendor \
     android.hardware.power@1.3.vendor \
-    android.hardware.power-V1-ndk_platform.vendor
+    android.hardware.power-service-mediatek \
 
 # Radio
 PRODUCT_PACKAGES += \
