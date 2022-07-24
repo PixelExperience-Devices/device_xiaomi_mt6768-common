ad08e9e1fa28604f1cb23a3923d770498fa2fde6
bengris32 <bengris32@protonmail.ch>
2022-07-24 22:15:12 +0100

    mt6768-common: dt2w: Drop DT2W service * Not needed anymore as we switched to custom power AIDL   that supports native control.
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I985e1e02f5139989ef55b7784786e1b1e6fbbd83

--- mt6768-common.mk
+++ mt6768-common.mk
@@ -123,13 +123,6 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
     android.hardware.drm@1.4.vendor
 
-# DT2W
-PRODUCT_PACKAGES += \
-    DT2W-Service-MT6768
-
-PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/dt2w/dt2w_event:$(TARGET_COPY_OUT_VENDOR)/bin/dt2w_event
-
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-service \
