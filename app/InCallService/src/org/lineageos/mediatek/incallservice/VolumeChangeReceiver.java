2d33a778326dbddf598487f25548d105971f3306
LinkBoi00 <linkdevel@protonmail.com>
2022-08-11 15:28:24 +0300

    fixup! mt6768-common: InCallService: Only adjust gain values for speaker and earpiece
    Replace 'OR' operators with logical 'AND' ones to fix the original mistake
    
    Signed-off-by: LinkBoi00 <linkdevel@protonmail.com>
    Change-Id: Ifa95cb994a50826998b3de5d9ba5830e6b4a87ac

--- app/InCallService/src/org/lineageos/mediatek/incallservice/VolumeChangeReceiver.java
+++ app/InCallService/src/org/lineageos/mediatek/incallservice/VolumeChangeReceiver.java
@@ -22,7 +22,7 @@ public class VolumeChangeReceiver extends BroadcastReceiver {
     private void handleVolumeStateChange(Intent intent) {
         if (intent.getIntExtra(AudioManager.EXTRA_VOLUME_STREAM_TYPE, -1) == AudioManager.STREAM_VOICE_CALL) {
             AudioDeviceInfo callDevice = mAudioManager.getCommunicationDevice();
-            if (callDevice.getType() != AudioDeviceInfo.TYPE_BUILTIN_EARPIECE || callDevice.getType() != AudioDeviceInfo.TYPE_BUILTIN_SPEAKER) {
+            if (callDevice.getType() != AudioDeviceInfo.TYPE_BUILTIN_EARPIECE && callDevice.getType() != AudioDeviceInfo.TYPE_BUILTIN_SPEAKER) {
                 // Device is not the built in earpiece or the speaker, we don't need to do anything.
                 return;
             }
