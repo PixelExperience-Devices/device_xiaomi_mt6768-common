43e307c9f6a9c77dbe6901503bac57aef9927b2a
bengris32 <bengris32@protonmail.ch>
2022-08-20 15:46:13 +0100

    mt6768-common: device: Build vendor variant of `android.hardware.gnss-V1-ndk` * CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.gnss-service.mediatek": library "android.hardware.gnss-V1-ndk.so" not found: needed by main executable
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I1ece553b5368da31bbba85572f278ac545a6ed99

--- mt6768-common.mk
+++ mt6768-common.mk
@@ -137,7 +137,8 @@ PRODUCT_PACKAGES += \
     android.hardware.gnss.measurement_corrections@1.1.vendor \
     android.hardware.gnss.visibility_control@1.0.vendor \
     android.hardware.gnss@2.0.vendor \
-    android.hardware.gnss@2.1.vendor
+    android.hardware.gnss@2.1.vendor \
+    android.hardware.gnss-V1-ndk.vendor
 
 # Health
 PRODUCT_PACKAGES += \
