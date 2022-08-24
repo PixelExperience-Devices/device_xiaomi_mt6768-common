e4d5960b040efdd98f750a5d9a02184228a26a4b
Robert Shih <robertshih@google.com>
2022-08-24 09:56:39 +0100

    mt6768-common: Enable AIDL Clearkey HAL
    Change-Id: I06c1736de2c6a4483d286fdba10bf3de26f8db81

--- mt6768-common.mk
+++ mt6768-common.mk
@@ -119,7 +119,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl:64 \
     android.hardware.drm@1.0-service-lazy \
-    android.hardware.drm@1.4-service.clearkey \
+    android.hardware.drm-service.clearkey \
     android.hardware.drm@1.0.vendor \
     android.hardware.drm@1.1.vendor \
     android.hardware.drm@1.2.vendor \
