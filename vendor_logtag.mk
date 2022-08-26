50c50b7302f17e9b269564378e058b090ffe6318
bengris32 <bengris32@protonmail.ch>
2022-08-26 12:47:31 +0100

    mt6768-common: configs: props: Silence gralloc4 logspam * Since Android 13, gralloc has been spamming the logs with: E gralloc4: Empty SMPTE 2094-40 data
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: Ib4d44443499f1396453bd7070aa1935485b329db

--- vendor_logtag.mk
+++ vendor_logtag.mk
@@ -254,4 +254,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.log.tag.hwcomposer=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.libnfc_nci=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.libPowerHal=$(VENDOR_LOG_LEVEL) \
-    persist.log.tag.mTEE=$(VENDOR_LOG_LEVEL)
+    persist.log.tag.mTEE=$(VENDOR_LOG_LEVEL) \
+    persist.log.tag.gralloc4=$(VENDOR_LOG_LEVEL)
